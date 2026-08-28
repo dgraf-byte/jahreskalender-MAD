42bf793ff5f84a8a0d42cfa61085802956b7810e
dgraf-byte <297474400+dgraf-byte@users.noreply.github.com>
2026-08-28 21:31:08 +0200

    komma

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
   SUPABASE_URL: "https://jfgagmybcajjevhpiino.supabase.co",
-  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_962MT6WvtUh-vbtU1NNP_A_pbz4enV8"
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_962MT6WvtUh-vbtU1NNP_A_pbz4enV8",
   VAPID_PUBLIC_KEY: "BKNCPUVskXCWIdiALHQVO63pH_S0JafQ7_sV_kiNpsu9R3KUXzka60nMoLv7buVK-nqN6TCw1lP8kLzq4CC8l1U"
 };
